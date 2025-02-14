b56bb9465c5f525f28b9147b310343f99ae3bb85
Gianlu201 <gianlucadidiego2001@gmail.com>
2025-02-14 11:16:42 +0100

    almost done

--- Progetto_S13-L5/Models/Contributor.cs
+++ Progetto_S13-L5/Models/Contributor.cs
@@ -6,5 +6,76 @@ using System.Threading.Tasks;
 
 namespace Progetto_S13_L5.Models
 {
-    public class Contributor { }
+    public class Contributor
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateTime BirthDay { get; set; }
+        public string CF { get; set; }
+        public string Gender { get; set; }
+        public string TownOfResidence { get; set; }
+        public double AnnualIncome { get; set; }
+
+        protected const int stagger1 = 15000;
+        protected const int stagger2 = 28000;
+        protected const int stagger3 = 55000;
+        protected const int stagger4 = 75000;
+
+        public Contributor(
+            string name,
+            string surname,
+            DateTime birthday,
+            string fiscalCode,
+            string gender,
+            string residence,
+            double income
+        )
+        {
+            Name = name;
+            Surname = surname;
+            BirthDay = birthday;
+            CF = fiscalCode;
+            Gender = gender;
+            TownOfResidence = residence;
+            AnnualIncome = income;
+        }
+
+        public double CalculateTax()
+        {
+            double tax = 0;
+            switch (AnnualIncome)
+            {
+                case <= stagger1:
+                    tax = AnnualIncome * 0.23;
+                    return tax;
+
+                case <= stagger2:
+                    tax = 3450 + (AnnualIncome - stagger1) * 0.27;
+                    return tax;
+
+                case <= stagger3:
+                    tax = 6960 + (AnnualIncome - stagger2) * 0.38;
+                    return tax;
+
+                case <= stagger4:
+                    tax = 17220 + (AnnualIncome - stagger3) * 0.41;
+                    return tax;
+
+                default:
+                    tax = 25420 + (AnnualIncome - stagger4) * 0.43;
+                    return tax;
+            }
+        }
+
+        public void ShowAllInfos()
+        {
+            Console.Clear();
+            Console.WriteLine($"Contributor: {Name} {Surname},");
+            Console.WriteLine($"born on {BirthDay.ToShortDateString()} ({Gender}),");
+            Console.WriteLine($"resident in {TownOfResidence},");
+            Console.WriteLine($"fiscal code: {CF}");
+            Console.WriteLine($"Declared income: €{AnnualIncome}");
+            Console.WriteLine($"TAX TO PAY: €{CalculateTax()}");
+        }
+    }
 }
--- Progetto_S13-L5/Program.cs
+++ Progetto_S13-L5/Program.cs
@@ -1,1 +1,48 @@
-﻿
+﻿using Progetto_S13_L5.Models;
+
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+Console.WriteLine("====== WELCOME IN EPITAX ======\n");
+
+Console.Write("What's your name: ");
+string name = Console.ReadLine();
+
+Console.Clear();
+Console.WriteLine("====== WELCOME IN EPITAX ======\n");
+Console.Write("What's your surname: ");
+string surname = Console.ReadLine();
+
+Console.Clear();
+Console.WriteLine("====== WELCOME IN EPITAX ======\n");
+Console.Write("What's your birthday (DD/MM/YYYY): ");
+string birthday = Console.ReadLine();
+
+Console.Clear();
+Console.WriteLine("====== WELCOME IN EPITAX ======\n");
+Console.Write("What's your fiscal code: ");
+string fiscalCode = Console.ReadLine();
+
+Console.Clear();
+Console.WriteLine("====== WELCOME IN EPITAX ======\n");
+Console.Write("What's your gender (M/F/O): ");
+string gender = Console.ReadLine();
+
+Console.Clear();
+Console.WriteLine("====== WELCOME IN EPITAX ======\n");
+Console.Write("What's your town of residence: ");
+string residence = Console.ReadLine();
+
+Console.Clear();
+Console.WriteLine("====== WELCOME IN EPITAX ======\n");
+Console.Write("What's your annual income: ");
+string income = Console.ReadLine();
+
+var test = DateTime.Parse(birthday);
+
+double incomeConv = double.Parse(income);
+
+var user = new Contributor(name, surname, test, fiscalCode, gender, residence, incomeConv);
+
+user.ShowAllInfos();
+
+Console.ReadKey();
