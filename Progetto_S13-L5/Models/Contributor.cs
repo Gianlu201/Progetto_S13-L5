9f74957dd366b3a1af179ff05ab3ab74e3aea6be
Gianlu201 <gianlucadidiego2001@gmail.com>
2025-02-14 09:40:52 +0100

    Aggiungere i file di progetto.

---
+++ Progetto_S13-L5/Models/Contributor.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Progetto_S13_L5.Models
+{
+    public class Contributor { }
+}
